bb8eaec156478ebfa4e2cff0b78f876260b19988
David Paparo <d,paparo@tiscali.it>
2025-07-18 15:22:49 +0200

    carico esercizio

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php
+
+
+
+//integer
+$number = 100;
+
+//float
+$number_x = 10.5;
+
+//string
+$name = "David";
+
+//boolean
+$check = false;
+
+var_dump($number , $number_x , $name , $check );
+
+
