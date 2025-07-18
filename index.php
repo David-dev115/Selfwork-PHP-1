78330f31757c00b8bf9dfcf0cb535554f1dc3a6c
David Paparo <d,paparo@tiscali.it>
2025-07-18 15:41:41 +0200

    eseguo correzione

--- index.php
+++ index.php
@@ -3,20 +3,43 @@
 
 
 
-//integer
-$number = 100;
+// //integer associato a una variabile:
+// $number = 100;
 
-//float
-$number_x = 10.5;
+// //float associato a una variabile:
+// $number_x = 10.5;
 
-//string
-$name = "David";
+// //string associato a una variabile:
+// $name = "David";
 
-//boolean
-$check = false;
+// //boolean associato a una variabile:
+// $check = false;
 
-var_dump($number , $number_x , $name , $check );
+
+//integer associato a una costante:
+const NUMBER = 100;
+
+//float associato a una costante:
+const NUMBER_X = 10.5;
+
+//string associato a una costante:
+const NAME = "David";
+
+//boolean associato a una costante:
+const CHECK = false;
+
+
+
+
+//METODO var_dump sulle variabili
+// var_dump($number , $number_x , $name , $check );
+
+
+// METODO var_dump sulle costanti
+var_dump( NUMBER , NUMBER_X , NAME , CHECK );
+
 
 //carico esercizio
+// carico esercizio corretto
 
 
